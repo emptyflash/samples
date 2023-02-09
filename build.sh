1a7d224a92367d385614c14b7a8d62e87e300fe2
Cameron Alexander <emptyflash@gmail.com>
2023-02-09 11:18:20 -0500

    Add more samples and fix build

--- build.sh
+++ build.sh
@@ -4,24 +4,32 @@ shopt -s extglob
 
 echo "{" > strudel.json
 echo '"_base": "https://raw.githubusercontent.com/emptyflash/samples/main/",' >> strudel.json
-LAST_DIRECTORY=""
-for d in */; do
-    echo "\"${d%\/}\": [" >> strudel.json
-    for f in $d* ; do
+
+process_files() {
+    LAST_FILE=""
+    for f in $1* ; do
         if [ ! -z $LAST_FILE ]; then
             echo "\"$LAST_FILE\"," >> strudel.json
         fi
         LAST_FILE="$f"
     done
     if [ ! -z $LAST_FILE ]; then
-            echo "\"$LAST_FILE\"" >> strudel.json
+        echo "\"$LAST_FILE\"" >> strudel.json
     fi
+}
+
+LAST_DIRECTORY=""
+for d in */; do
     if [ ! -z $LAST_DIRECTORY ]; then
+        echo "\"${LAST_DIRECTORY%\/}\": [" >> strudel.json
+        process_files "$LAST_DIRECTORY"
         echo "]," >> strudel.json
     fi
     LAST_DIRECTORY="$d"
 done
 if [ ! -z $LAST_DIRECTORY ]; then
+    echo "\"${LAST_DIRECTORY%\/}\": [" >> strudel.json
+    process_files "$LAST_DIRECTORY"
     echo "]" >> strudel.json
 fi
 echo "}" >> strudel.json
